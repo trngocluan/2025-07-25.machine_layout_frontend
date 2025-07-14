f5af7ba82dbaa33465dc5d2c10c4af0c10690cd8
Tran Ngoc Luan <44539564+trngocluan@users.noreply.github.com>
2025-07-14 10:23:07 +0700

    Sửa lại frontend theo phương án API đã reset: http://localhost:3000/machine-performance?factory=2 --> VD: "machine_no": 2201,             "status": 0,             "x": 2013,             "y": 1394,             "machine_type": 10,             "ct": 50

--- src/app/models/machine.model.ts
+++ src/app/models/machine.model.ts
@@ -1,10 +1,16 @@
 // src/app/models/machine.model.ts
 // Đây là interface định nghĩa cấu trúc dữ liệu máy
 export interface Machine {
-  id: number;           // ID của máy
-  name: string;         // Tên máy
+  //id: number;           // ID của máy
+  //name: string;         // Tên máy
+  //status: number;       // Trạng thái máy: 0 (stop), 1 (running), 2 (error)
+  //highlight: boolean;   // Cờ đánh dấu nổi bật
+  //x: number;            // Tọa độ X trên layout
+  //y: number;            // Tọa độ Y trên layout
+  machine_no: string;           // tên máy
   status: number;       // Trạng thái máy: 0 (stop), 1 (running), 2 (error)
-  highlight: boolean;   // Cờ đánh dấu nổi bật
   x: number;            // Tọa độ X trên layout
   y: number;            // Tọa độ Y trên layout
+  machine_type: string;         // Loại máy (ví dụ: "Milling", "Lathe")
+  ct: number;          // Thời gian chu kỳ (cycle time) của máy
 }
