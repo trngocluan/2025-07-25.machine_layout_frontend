a751b940ece83d572152a47677048e5f62641b8d
Tran Ngoc Luan <44539564+trngocluan@users.noreply.github.com>
2025-07-16 14:07:52 +0700

    Thêm giao diện tạm thời cho 3 trang con, và khai báo trong app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { MercuryComponent } from './mercury/mercury.component';
+import { TierraComponent } from './tierra/tierra.component';
+import { JupiterComponent } from './jupiter/jupiter.component';
+import { SaturnComponent } from './saturn/saturn.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'mercury', pathMatch: 'full' },
   { path: 'mercury', component: MercuryComponent },
+  { path: 'tierra', component: TierraComponent },
+  { path: 'jupiter', component: JupiterComponent },
+  { path: 'saturn', component: SaturnComponent },
 ];
